28c5cadd318051e1a365dbb726ca95032a1e36d1
StefanDeSterke <stefan6kamphuis@gmail.com>
2023-10-28 16:03:54 +0200

    Added a data retriever

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from astroquery.gaia import Gaia
+from util.gaia_retriever import async_retrieve_astrophysical_parameters as retrieve_parameters
+import config
 
 Gaia.ROW_LIMIT = -1
-Gaia.login(user="", password="")
+Gaia.login(user=config.user, password=config.password)
+
+QUERY = open("").read()
+
+parameters = retrieve_parameters(QUERY)
+
+print(parameters)
---
+++ sql/spectraltype_teff.sql
@@ -0,0 +1,7 @@
+SELECT gaiadr3.astrophysical_parameters.spectraltype_esphs,gaiadr3.astrophysical_parameters.teff_esphs
+FROM gaiadr3.astrophysical_parameters
+INNER JOIN gaiadr3.gaia_source ON gaiadr3.astrophysical_parameters.source_id=gaiadr3.gaia_source.source_id
+WHERE gaiadr3.astrophysical_parameters.spectraltype_esphs IS NOT NULL
+AND gaiadr3.astrophysical_parameters.spectraltype_esphs != 'unknown'
+AND gaiadr3.astrophysical_parameters.teff_esphs IS NOT NULL
+AND gaiadr3.gaia_source.random_index BETWEEN 0 AND 500
